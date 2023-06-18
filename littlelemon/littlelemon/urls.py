0fe3a3066ea48a5f1dc1a2b44b1c5353442667df
Matthew Elmajian <matthewelmajian@Matthews-MBP.attlocal.net>
2023-06-18 06:29:38 -0700

    Added authentication steps

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,11 +19,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 
+
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('api/', include('restaurant.urls')),
     path('admin/', admin.site.urls),
-    path('restaurant/booking/', include(router.urls))
+    path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
